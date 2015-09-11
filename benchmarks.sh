9a8b1fac7823934502c93c504f91d275a025a289
Martin Kouba <mkouba@redhat.com>
2015-09-11 21:33:19 +0200

    1.8.2.Final

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
-TRIMOU_VERSIONS="1.6.2.Final 1.7.3.Final 1.8.1.Final";
-#TRIMOU_VERSIONS="1.6.2.Final";
-#TRIMOU_VERSIONS="1.7.3.Final";
-#TRIMOU_VERSIONS="1.8.1-SNAPSHOT";
+#TRIMOU_VERSIONS="1.8.2.Final 1.8.3-SNAPSHOT";
+TRIMOU_VERSIONS="1.6.2.Final 1.7.3.Final 1.8.1.Final 1.8.2.Final";
 
-echo $TRIMOU_VERSIONS;
+# Set max to use Runtime.getRuntime().availableProcessors()
+THREADS="1"
+
+# Benchmarks to run
+if [ "$1" ]; then
+    BENCHMARKS=$1
+else
+    BENCHMARKS="Basic|Helper|Partial|TemplateInheritance"
+fi
+
+echo "Trimou versions to test: $TRIMOU_VERSIONS";
+echo "Benchmarks to run: $BENCHMARKS"
+
 TRIMOU_VERSIONS_ARRAY=$(echo $TRIMOU_VERSIONS);
 
 for i in $TRIMOU_VERSIONS_ARRAY
 do
   mvn package -Dversion.trimou=$i
-  java -jar target/trimou-benchmarks.jar -rf json -rff target/results-$i.json
+  java -jar target/trimou-benchmarks.jar -t $THREADS -rf json -rff target/results-$i.json $BENCHMARKS
   java -cp target/trimou-benchmarks.jar org.trimou.benchmark.chart.ChartGenerator target
 done;
